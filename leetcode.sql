623024e7e42c0b558733a3760ea2695af3c984df
murtiunlimited <murtaza32004@gmail.com>
2024-12-24 00:53:05 -0800

    Update leetcode.sql

--- leetcode.sql
+++ leetcode.sql
@@ -76,4 +76,11 @@ HAVING COUNT(TableB.id)>=5
 SELECT * FROM Cinema
 WHERE (id%2=1) AND (description <> 'boring')
 ORDER BY rating DESC 
-
+-- 16
+SELECT Prices.product_id, 
+       COALESCE(ROUND((SUM(UnitsSold.units * Prices.price)) / (SUM(UnitsSold.units)),2),0) AS average_price
+FROM Prices
+LEFT JOIN UnitsSold
+    ON Prices.product_id = UnitsSold.product_id
+    AND UnitsSold.purchase_date BETWEEN Prices.start_date AND Prices.end_date
+GROUP BY Prices.product_id;
