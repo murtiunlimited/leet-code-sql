b3c4b794a7c55db22fa9df4ce5ea11963ad24ce9
murtiunlimited <murtaza32004@gmail.com>
2024-12-24 23:18:28 -0800

    Update leetcode.sql

--- leetcode.sql
+++ leetcode.sql
@@ -97,3 +97,12 @@ ROUND((COUNT(Register.contest_id) / (SELECT count(Users.user_id) FROM Users))*10
 FROM Register
 GROUP BY contest_id
 ORDER BY percentage DESC, contest_id;
+-- 19
+SELECT
+query_name,
+ROUND(SUM(rating/position) / COUNT(query_name),2) AS quality,
+ROUND((SUM(if(rating<3,1,0)) / COUNT(query_name))*100,2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name
+
+
